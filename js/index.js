14bf02510a0a4d50f4d3787d2a910ee09610fea2
anushahasti <anusha.hasthi@gmail.com>
2021-02-16 01:50:33 -0700

    Updated spanish
    Updated spanish page and last section of home also added anchor tags

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$('.scrollTo').click(function(){
+
+    var getElement= $(this).attr('href');
+    if($(getElement).length){
+        var getOffset = $(getElement).offset().top;
+        $('html, body').animate({
+            scrollTop: getOffset
+        }, 500);
+    }
+    return false;
+
+});
+
